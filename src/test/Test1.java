a89035607ca346d40ef7646ec691ddf9438f366e
abdosalehh <abdelrahman.saleh@si-ware.com>
2022-08-16 14:27:27 +0200

    initial commit

---
+++ src/test/Test1.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.io.PrintWriter;
+
+public class Test1 {
+	public static void main(String[] args) {
+		PrintWriter pw = new PrintWriter(System.out);
+		pw.println("Learning Git");
+		pw.flush();
+	}
+}
